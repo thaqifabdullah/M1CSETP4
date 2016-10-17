b01745ee38f342714f8b838fec9560250a999ff3
thaqifabdullah <thaqifabdullah@gmail.com>
2016-10-17 19:46:02 +0200

    update philosophe

--- philosophe.c
+++ philosophe.c
@@ -4,60 +4,79 @@
 #include <sys/types.h>
 #include <unistd.h>
 #define N 5 // nombre de philosophes = nombre de baguettes 
-#define PORTION 10
-int bag_dispo[N];
+#define PORTION 5
+int baguette_dispo[N];
 pthread_mutex_t mutex;
-pthread_cond_t *cond;
+pthread_cond_t cond[N];
 
-typedef struct philo{
-	int nbr_portion;
-	int indice_philo;
-}philo_t;
+void prendre_baguettes(int i){
+	pthread_mutex_lock(&mutex);
+	while(!baguette_dispo[i] || !baguette_dispo[(i+1)%N]){
+		if(!baguette_dispo[i])
+			pthread_cond_wait(&cond[i],&mutex);
+		else if(!baguette_dispo[(i+1)%N])
+			pthread_cond_wait(&cond[(i+1)%N],&mutex);
+		else{
+			pthread_cond_wait(&cond[i],&mutex);
+			pthread_cond_wait(&cond[(i+1)%N],&mutex);
+		}
+	}
+	baguette_dispo[i] = 0;
+	baguette_dispo[(i+1)%N] = 0;
+	pthread_mutex_unlock(&mutex);
+}
+
+void reposer_baguettes(int i){
+	pthread_mutex_lock(&mutex);
+	baguette_dispo[i] = 1;
+	baguette_dispo[(i+1)%N] = 1;
+	pthread_cond_broadcast(&cond[i]);
+	pthread_cond_broadcast(&cond[(i+1)%N]);
+	pthread_mutex_unlock(&mutex);
+}
+
+int manger(int i){
+	return i--;
+}
 
 void *philosophe(void *arg){
-	philo_t *p = (void *)arg;
-	tid = pthread_self () ;
-  	srand ((int) tid) ;
-	usleep (rand() / RAND_MAX * 1000000.);
-	pthread_mutex_lock(&mutex);
-		while(bag_dispo[p->indice_philo] || bag_dispo[(p->indice_philo+1)%N]){
-			bag_dispo[p->indice_philo] ? pthread_cond_wait(&cond[p->indice_philo]) :  pthread_cond_wait(&cond[(p->indice_philo+1)%N]);
-		}
-	bag_dispo[arg->indice_philo] = 1;
-	bag_dispo[(arg->indice_philo+1)%N] = 1;
-	pthread_mutex_unlock(&mutex);
-	arg->nbr_portion--; //Philosophe mange une portion
-	printf("Philosophe no %d a mangé une portion. Il reste %d portion\n", arg->indice_philo, arg->nbr_portion);
-	pthread_mutex_lock(&mutex);
-	bag_dispo[arg->indice_philo] = 0;
-	bag_dispo[(arg->indice_philo+1)%N] = 0;
-	pthread_cond_broadcast(&cond);
-	pthread_mutex_unlock(&mutex);
+	int indice_philo = *(int *)arg;
+	int nbr_portion = 5;
+	pthread_t tid;
+	tid = pthread_self();
+	while(nbr_portion){
+  		srand ((int) tid) ;
+		usleep (rand() / RAND_MAX * 1000000.);
+		printf("Thread %x a faim\n", (unsigned int)tid);
+		prendre_baguettes(indice_philo);
+		nbr_portion = manger(nbr_portion);
+		printf("Thread %x a mangé une portion\n", (unsigned int)tid);
+		reposer_baguettes(indice_philo);
+	}
 	return (void *) tid;
 }
 
 
 int main(int argc, char const *argv[])
 {
-	pthread_mutex_init(&mutex,NULL);
+	if(pthread_mutex_init(&mutex,NULL) != 0){
+		fprintf(stderr, "Mutex init error\n");
+		return -1;
+	}
 	for(int i=0; i<N; i++){
 		pthread_cond_init(&cond[i],NULL);			
 	}
-	// initialiser le tableau de bag_dispo. Toutes les baguettes sont disponible
+	// initialiser le tableau de baguette_dispo. Toutes les baguettes sont disponible
 	for(int i=0; i<N; i++){
-		bag_dispo[i] = 0;
+		baguette_dispo[i] = 1;
 	}
 
 	pthread_t *tids ;
 	tids = malloc(sizeof(pthread_t)*N);
 
-	philo_t *philos;
-	philos = malloc(sizeof(struct philo)*N);
-
 	for(int i = 0 ;i <N ;i++){
-		philos->nbr_portion = PORTION;
-		philos->indice_philo = i;
-		pthread_create(&tids[i],NULL,philosophe,&philos[i]);
+		printf("create pthread\n");
+		pthread_create(&tids[i],NULL,philosophe,&i);
 	}
 
 	for(int j = 0;j <N ; j++){
@@ -65,7 +84,6 @@ int main(int argc, char const *argv[])
 	}
 
 	free(tids);
-	free(philos);
 
 	return 0;
 }
